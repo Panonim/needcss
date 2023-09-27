39252f06b41d4d8f175bbb3b6f4dd60557b88bb1
Panonim <106886746+ViiiBOT@users.noreply.github.com>
2023-09-27 17:04:36 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,4 @@
-
 const savedTheme = localStorage.getItem("theme");
-
 
 function setTheme(theme) {
     if (theme === 'system-mode') {
@@ -10,30 +8,26 @@ function setTheme(theme) {
     
     document.documentElement.className = theme;
     localStorage.setItem("theme", theme);
+    
+    const hdr = document.getElementById('hdr');
     if (theme === 'light-mode') {
-        document.getElementById('hdr').style.backgroundColor = 'rgb(215, 215, 215, 0.2)';
+        hdr.style.backgroundColor = 'rgb(215, 215, 215, 0.2)';
         document.documentElement.style.setProperty('--hd-color', 'rgb(215, 215, 215, 0.2)');
+    } else if (theme === 'dark-mode') {
+        hdr.style.backgroundColor = 'rgba(0, 0, 0, 0.2)';
+        document.documentElement.style.setProperty('--hd-color', 'rgba(0, 0, 0, 0.2)');
     }
-  
-  else if (theme === 'dark-mode') {
-        document.getElementById('hdr').style.backgroundColor = 'rgba(255, 255, 255, 0.2)';
-        document.documentElement.style.setProperty('--hd-color', 'rgba(255, 255, 255, 0.2)');
-    }
-
-  
-  
 
     const ncssLogo = document.getElementById('ncsslogo');
     if (theme === 'light-mode') {
-        // Zmień jasność obrazka na 0% 
+        // Set image brightness to 0%
         ncssLogo.style.filter = 'brightness(0%)';
     } else if (theme === 'dark-mode') {
-        // Zmień kolor obrazka na #333333 
+        // Set image color to #333333 and reset brightness
         ncssLogo.style.filter = 'brightness(100%)';
-        ncssLogo.style.fill = '#333333';
+        ncssLogo.style.fill = '#333333'; // Assuming 'ncsslogo' is an SVG image
     }
 }
-
 
 if (savedTheme) {
     setTheme(savedTheme);
